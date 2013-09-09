26bb0767e96a4d2861a55556b259f146c762ed6d
Stefanie <stefanie.drost@gmx.de>
2013-09-09 10:18:29 +0200

    remove existing image when uploading new banner image

--- app/code/community/Sdrost/Responsive/controllers/Adminhtml/SliderController.php
+++ app/code/community/Sdrost/Responsive/controllers/Adminhtml/SliderController.php
@@ -10,6 +10,10 @@
 class Sdrost_Responsive_Adminhtml_SliderController extends Mage_Adminhtml_Controller_Action
 {
 
+    protected $postData;
+    protected $urlPath;
+    protected $path;
+    
     public function indexAction()
     {
         $this->loadLayout();
@@ -18,98 +22,57 @@ class Sdrost_Responsive_Adminhtml_SliderController extends Mage_Adminhtml_Contro
 
     public function saveAction()
     {
-        if ($data = $this->getRequest()->getPost()) {
+        if ($this->data = $this->getRequest()->getPost()) {
 
+            $this->urlPath = Mage::getBaseUrl('media') . 'sliderimages';
+            $this->path = Mage::getBaseDir('media') . DS . 'sliderimages';
+            
             //check images
-            $this->checkForSliderImage($_FILES, 1, $data);
-            $this->checkForSliderImage($_FILES, 2, $data);
-            $this->checkForSliderImage($_FILES, 3, $data);
+            $this->checkForSliderImage($_FILES, 1);
+            $this->checkForSliderImage($_FILES, 2);
+            $this->checkForSliderImage($_FILES, 3);
             
-            
-            
-            
-            
-            
-//            if (isset($_FILES['slider_image2']['name']) &&
-//                    (file_exists($_FILES['slider_image2']['tmp_name']))) {
-//                try {
-//                    $uploader = new Varien_File_Uploader('slider_image2');
-//                    $uploader->setAllowedExtensions(array('jpg', 'jpeg', 'gif', 'png')); // or pdf or anything
-//                    $uploader->setAllowRenameFiles(false);
-//                    
-//                    $uploader->setFilesDispersion(false);
-//
-//                    $path = Mage::getBaseDir('media') . DS . 'sliderimages';
-//                    $urlPath = Mage::getBaseUrl('media') . 'sliderimages';
-//                    
-//                    $imageModel = Mage::getModel('responsive/sliderimages')->load(1);
-//                    $imageModel->addData(array(
-//                        'name'  =>  $_FILES['slider_image2']['name'],
-//                        'path'  =>  $urlPath . DS . $_FILES['slider_image1']['name']
-//                    ))->save();
-//                    
-//                    $uploader->save($path, $_FILES['slider_image2']['name']);
-//
-//                    $data['slider_image2'] = $_FILES['slider_image2']['name'];
-//                } catch (Exception $e) {
-//                    
-//                }
-//            } else {
-//
-//                if (isset($data['slider_image2']['delete']) && $data['slider_image2']['delete'] == 1)
-//                    $data['image_main'] = '';
-//                else
-//                    unset($data['slider_image2']);
-//            }
         }
         $this->_forward('index');
     }
     
-    private function checkForSliderImage($filesData, $imageId, $data)
+    private function checkForSliderImage($filesData, $imageId)
     {
         $fieldname = 'slider_image' . $imageId;
         
         if (isset($filesData[$fieldname]['name']) &&
                     (file_exists($filesData[$fieldname]['tmp_name']))) {
                 try {
-//                    var_dump($_FILES);
-                    $uploader = new Varien_File_Uploader($fieldname);
-                    $uploader->setAllowedExtensions(array('jpg', 'jpeg', 'gif', 'png')); // or pdf or anything
-                    $uploader->setAllowRenameFiles(false);
-//
-//                    // setAllowRenameFiles(true) -> move your file in a folder the magento way
-//                    // setAllowRenameFiles(true) -> move your file directly in the $path folder
-                    $uploader->setFilesDispersion(false);
-//
-                    $path = Mage::getBaseDir('media') . DS . 'sliderimages';
-                    $urlPath = Mage::getBaseUrl('media') . 'sliderimages';
+                    // define directory and url of image
+                    $imagePath = $this->path . DS . $filesData[$fieldname]['name'];
+                    $url = $this->urlPath . DS . $filesData[$fieldname]['name'];
                     
-//                    var_dump($urlPath . DS . $filesData[$fieldname]['name']);
-                    
-                    $imagePath = $path . DS . $filesData[$fieldname]['name'];
-                    $url = $urlPath . DS . $filesData[$fieldname]['name'];
-                    
+                    // save image in directory
+                    $this->saveSliderImage($fieldname, $filesData);
+        
+                    // new db entry
                     $imageModel = Mage::getModel('responsive/sliderimages')
                             ->load($imageId);
+                    
+                    // unlink existing image
+                    if(!is_null($imageModel->getPath())){
+                        unlink($imageModel->getPath());
+                    }
+                    
                     $imageModel->addData(array(
                         'name'  =>  $filesData[$fieldname]['name'],
                         'path'  =>  $imagePath,
                         'url'   =>  $url
                     ))->save();
-                    
-                    
-//                    var_dump($path);
-//                    var_dump($urlPath);
-                    $uploader->save($path, $filesData[$fieldname]['name']);
-
-                    $data[$fieldname] = $filesData[$fieldname]['name'];
                 } catch (Exception $e) {
                     
                 }
             } else {
-                if (isset($data[$fieldname]['delete']) && $data[$fieldname]['delete'] == 1){
+                if (isset($this->data[$fieldname]['delete']) && 
+                        $this->data[$fieldname]['delete'] == 1){
                     
-                    $data['image_main'] = '';
+                    $this->data['image_main'] = '';
+                    $this->deleteImage($fieldname);
                     
                     // update image entry
                     $imageModel = Mage::getModel('responsive/sliderimages')
@@ -119,15 +82,41 @@ class Sdrost_Responsive_Adminhtml_SliderController extends Mage_Adminhtml_Contro
                         'path'  =>  '',
                         'url'   =>  ''
                     ))->save();
-                    
-                    // unlink image file
-                    $url = Mage::getBaseUrl('media');
-                    $imagePath = Mage::getBaseDir('media') . DS . str_replace($url, "", $data[$fieldname]['value']);
-                    unlink($imagePath);
-                }  
+                } 
                 else{
-                    unset($data[$fieldname]);
-                }  
+                    unset($this->data[$fieldname]);
+                } 
             }
+    }
+    
+    /**
+     * Saves image into slider directory.
+     * 
+     * @param string $fieldname
+     * @param array $filesData
+     */
+    private function saveSliderImage($fieldname, $filesData)
+    {
+        $uploader = new Varien_File_Uploader($fieldname);
+        $uploader->setAllowedExtensions(array('jpg', 'jpeg', 'gif', 'png'));
+        $uploader->setAllowRenameFiles(false);
+        $uploader->setFilesDispersion(false);
+        $uploader->save($this->path, $filesData[$fieldname]['name']);
+
+        $this->data[$fieldname] = $filesData[$fieldname]['name'];
+    }
+    
+    /**
+     * Deletes image from slider directory.
+     * 
+     * @param string $fieldname
+     * @param integer $imageId
+     */
+    private function deleteImage($fieldname)
+    {             
+        // unlink image file
+        $url = Mage::getBaseUrl('media');
+        $imagePath = Mage::getBaseDir('media') . DS . str_replace($url, "", $this->data[$fieldname]['value']);
+        unlink($imagePath);         
     }
 }
