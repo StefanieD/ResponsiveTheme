65258cf75958d371cd4706485f6179c7e8b84107
Stefanie <stefanie.drost@gmx.de>
2013-09-27 20:31:43 +0200

    added function to category helper

--- app/code/community/Sdrost/Responsive/Helper/Categories.php
+++ app/code/community/Sdrost/Responsive/Helper/Categories.php
@@ -40,4 +40,34 @@ class Sdrost_Responsive_Helper_Categories extends Mage_Core_Helper_Abstract
     {
         return Mage::getBaseUrl('skin') . 'frontend/responsive/default/images' . DS . $this->defaultImage;
     }
+    
+    public function getCategoryImage(Mage_Catalog_Model_Category $category, $width = 250, $height = 180)
+    {
+        // return when no image exists
+        if (!$category->getImage()) {
+            return false;
+        }
+
+        // return when the original image doesn't exist
+        $imagePath = Mage::getBaseDir('media') . DS . 'catalog' . DS . 'category'
+                . DS . $category->getImage();
+        if (!file_exists($imagePath)) {
+            return false;
+        }
+
+        // resize the image if needed
+        $rszImagePath = Mage::getBaseDir('media') . DS . 'catalog' . DS . 'category'
+                . DS . 'cache' . DS . $width . 'x' . $height . DS
+                . $category->getImage();
+        if (!file_exists($rszImagePath)) {
+            $image = new Varien_Image($imagePath);
+            $image->resize($width, $height);
+            $image->save($rszImagePath);
+        }
+
+        // return the image URL
+        return Mage::getBaseUrl('media') . '/catalog/category/cache/' . $width . 'x'
+                . $height . '/' . $category->getImage();
+    }
+
 }
